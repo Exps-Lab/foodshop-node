9554e93e38e50dd06331bd6d7488481124d24eb2
haihan <haihanh2@163.com>
2022-04-06 17:28:47 +0800

    logger添加超上限删除

--- logger.js
+++ logger.js
@@ -1,5 +1,5 @@
 // 处理日志
-const { promises } = require('fs')
+const { mkdir, stat, appendFile, rename, writeFile, readdir, rm } = require('fs/promises')
 const { join } = require('path')
 const config = require('./conf/index')
 
@@ -42,26 +42,31 @@ class Logger {
     })
   }
   async writeLog (data) {
+    const maxStayDay = 1
     const { level, errMes, errInfo } = data
     const path = join(__dirname, this.logPath)
     const tempFileName = `${path}logs.log`
     const content = `${new Date().formatTime('yyyy-MM-dd hh:mm:ss')}; ${level}: ${errMes} ${errInfo || ''}`
 
-    // todo 上限删除
     try {
-      await promises.mkdir(path, { recursive: true })
-      await promises.stat(tempFileName).then(async (todayLogFile) => {
-        const fileTime = new Date(todayLogFile.ctimeMs)
+      await mkdir(path, { recursive: true })
+      await stat(tempFileName).then(async todayLogFile => {
+        const fileTime = new Date(todayLogFile.birthtimeMs)
         const timeFormat = fileTime.formatTime('yyyy-MM-dd')
-        if (new Date().toDateString === fileTime.toDateString) {
-          await promises.appendFile(tempFileName, `\r\n${content}`)
+        if (new Date().toDateString() === fileTime.toDateString()) {
+          await appendFile(tempFileName, `\r\n${content}`)
         } else {
-          await promises.rename(tempFileName, `${tempFileName}_${timeFormat}`)
-          await promises.writeFile(tempFileName, content)
+          await readdir(path).then(async dirFile => {
+            if (dirFile.length >= maxStayDay) {
+              await rm(dirFile[dirFile.length -1])
+            }
+            await rename(tempFileName, `${tempFileName}_${timeFormat}`)
+            await writeFile(tempFileName, content)
+          })
         }
       }).catch(async (err) => {
         if (err.errno === -2) {
-          await promises.writeFile(tempFileName, content)
+          await writeFile(tempFileName, content)
         }
       })
     } catch  (err) {
