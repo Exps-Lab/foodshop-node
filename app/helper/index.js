ddb33926e95dab3c333805b409fc3c51d0bdb0e7
wangxiaowei <1723882444@qq.com>
2022-10-08 22:55:38 +0800

    [UPDATE]: 更新距离返回值为number类型

--- app/helper/index.js
+++ app/helper/index.js
@@ -41,19 +41,18 @@ function validate (rule={}, req) {
  * @param lng1
  * @param lat2
  * @param lng2
- * @returns {string}
- * @constructor
+ * @returns {number}
  */
 function getTwoPosDistance(lat1,  lng1,  lat2,  lng2){
-  const radLat1 = lat1*Math.PI / 180.0;
-  const radLat2 = lat2*Math.PI / 180.0;
+  const radLat1 = lat1 * Math.PI / 180.0;
+  const radLat2 = lat2 * Math.PI / 180.0;
   const a = radLat1 - radLat2;
-  const  b = lng1*Math.PI / 180.0 - lng2*Math.PI / 180.0;
-  let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
-    Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
-  s = s *6378.137 ; // EARTH_RADIUS;
+  const b = (lng1 * Math.PI / 180.0) - (lng2 * Math.PI / 180.0);
+  let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+    Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+  s = s * 6378.137 ; // EARTH_RADIUS;
   s = Math.round(s * 10000) / 10000;
-  return s.toFixed(2)
+  return Number(s.toFixed(2))
 }
 
 module.exports = {
