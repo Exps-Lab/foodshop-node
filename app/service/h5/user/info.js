c54106d63c54cb41461ef530c948c3831cb4eedd
haihan <haihanh2@163.com>
2023-02-12 20:46:50 +0800

    perf: log优化

--- app/service/h5/user/info.js
+++ app/service/h5/user/info.js
@@ -42,38 +42,42 @@ class UserInfoService extends LoginBase {
     }
   }
 
+  // 更新用户名
   async updateUserName (req, res) {
     const { u_id } = req.session
     const { username } = req.body
-    const { RedisInstance } = _common
-    try {
-      const resData = await UserModel.findOneAndUpdate({ u_id }, { username }, { new: true }).lean(true)
-      await RedisInstance.hSet(`${this.h5UserInfoPreKey}:${u_id}`, 'username', username)
-      res.json({
-        data: resData,
-        msg: 'succeed!'
-      })
-    } catch (err) {
-      res.json({
-        code: 20002,
-        resData: err
-      })
-    }
+    const resData = await this.updateUserField(u_id, 'username', username)
+    res.json(resData)
   }
 
+  // 更新头像
   async updateUserAvatar (req, res) {
     const { u_id } = req.session
     const { avatar } = req.body
+    const resData = await this.updateUserField(u_id, 'avatar', avatar)
+    res.json(resData)
+  }
+
+  // 更新（单个）用户信息工具函数
+  async updateUserField (u_id, key, value) {
     const { RedisInstance } = _common
     try {
-      const resData = await UserModel.findOneAndUpdate({ u_id }, { avatar }, { new: true }).lean(true)
-      await RedisInstance.hSet(`${this.h5UserInfoPreKey}:${u_id}`, 'avatar', avatar)
-      res.json({
+      const resData = await UserModel.findOneAndUpdate(
+        { u_id },
+        {
+          $set: {
+            [key]: value
+          }
+        },
+        { new: true }
+      ).lean(true)
+      await RedisInstance.hSet(`${this.h5UserInfoPreKey}:${u_id}`, key, value)
+      return({
         data: resData,
         msg: 'succeed!'
       })
     } catch (err) {
-      res.json({
+      return({
         code: 20002,
         resData: err
       })
