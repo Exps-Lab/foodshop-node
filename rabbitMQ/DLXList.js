44d03d4a69bbcd23b6be71f7868aad1b4933d7ff
haihan <haihanh2@163.com>
2023-10-11 10:25:56 +0800

    doc: 完善mq使用说明

--- rabbitMQ/DLXList.js
+++ rabbitMQ/DLXList.js
@@ -1,5 +1,15 @@
-// MQ队列配置
-// 需要新增直接配置即可，之后会在index里统一初始化mq实例
+
+/**
+ * MQ队列配置
+ *
+ * 新增：直接配置即可，之后会在index里统一初始化mq实例
+ * 支持：目前支持两类ttl的mq实例：
+ * 1. 消息队列统一设置过期时间
+ * 2. 队列不设置，每次在channel发消息时附加消息的过期时间
+ *
+ * note: 两种类型在配置时是根据 ExtraConf 配置项的 useQueueExpiredTime 字段区分，默认使用模式1
+ */
+
 
 const MQDLXList = {
   orderPay: {
